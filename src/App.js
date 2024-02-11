0d9eaa419101671f659da94254af201e8f810401
ankush <ankush.sundaresha@rhibhus.com>
2024-02-11 20:33:52 +0530

    tic-tac-toe using react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import LightDarkMode from './components/LightDarkMode';
+import TicTacToe from './components/TicTacToe';
+
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe />
+      {/* <LightDarkMode /> */}
+    </div>
+  );
+}
+
+export default App;
